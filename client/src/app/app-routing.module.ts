f0992fce01324cb13512e24532221488e582a10b
danyal-deekoders <deekoders@gmail.com>
2023-05-22 10:05:30 +0500

    about component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { CardsComponent } from "./cards/cards.component";
+import { AboutComponent } from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "about", component: AboutComponent },
+  { path: "", component: CardsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HttpClientModule],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CardsComponent } from './cards/cards.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { CardsComponent } from "./cards/cards.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, CardsComponent],
+  declarations: [AppComponent, NavbarComponent, CardsComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
